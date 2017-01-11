27a2f577a418e68d93a423b62eb48b61202b4df1
karl-arreola <karl_arreola@yahoo.ca>
2017-01-11 10:47:16 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Karl
+ * Date: 1/11/2017
+ * Time: 10:41 AM
+ */
+?>
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Index</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello out there... I live"
+        ?>
+    </body>
+</html>
